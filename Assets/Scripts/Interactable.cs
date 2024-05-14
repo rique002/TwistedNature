a9e08ed104764516ff0dc649ebad450cc4dfcc48
Henrique Ferreira <henriqueferreira.2002@outlook.pt>
2024-05-14 18:08:06 +0100

    Refactor

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,39 +3,31 @@ using PlayableCharacters;
 
 public class Interactable : MonoBehaviour
 {
-    [SerializeField]
-    protected float interactionDistance = 3f;
+    [SerializeField] protected float interactionDistance = 3f;
 
-    [SerializeField]
-    protected KeyCode interactionKey = KeyCode.E;
+    [SerializeField] protected KeyCode interactionKey = KeyCode.E;
 
-    [SerializeField]
-    protected string interactionPrompt = "Press E to interact";
+    [SerializeField] protected string interactionPrompt = "Press E to interact";
 
-    [SerializeField]
-    protected PlayableCharacter playableCharacter;
+    [SerializeField] protected PlayableCharacter playableCharacter;
 
     public virtual void Interact()
     {
-
         Debug.Log("Interacting with " + gameObject.name);
     }
 
     protected virtual void Update()
     {
-
-        if (Vector3.Distance(transform.position, playableCharacter.gameObject.transform.position) <= interactionDistance)
+        if (Vector3.Distance(transform.position, playableCharacter.gameObject.transform.position) <=
+            interactionDistance)
         {
-
             Debug.Log(interactionPrompt);
 
 
             if (Input.GetKeyDown(interactionKey))
             {
-
                 Interact();
             }
         }
     }
 }
-
